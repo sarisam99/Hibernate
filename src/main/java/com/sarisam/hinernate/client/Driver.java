e7cd61de46cb6fc8bdff2d70ff9bd1e8e43cde69
sarisam99 <sariputra9999@gmail.com>
2016-03-03 19:59:04 +0530

    Main class chenges
    Main class chenges

---
+++ src/main/java/com/sarisam/hinernate/client/Driver.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.sarisam.hinernate.client;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+import com.sarisam.hinernate.entity.Employee;
+
+/**
+ * @author lingadal
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		//SessionFactory sessionFactory=new AnnotationConfiguration().configure().buildSessionFactory();
+		Employee employee=new Employee();
+		employee.setEmployeeName("sariputra");
+		System.out.println(employee);
+	}
+
+}
---
+++ src/main/java/com/sarisam/hinernate/entity/Employee.java
@@ -0,0 +1,71 @@
+package com.sarisam.hinernate.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Employee")
+public class Employee {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String employeeName;
+	private  double salary;
+	private Date dob;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int id, String employeeName, double salary, Date dob) {
+		super();
+		this.id = id;
+		this.employeeName = employeeName;
+		this.salary = salary;
+		this.dob = dob;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", employeeName=" + employeeName + ", salary=" + salary + ", dob=" + dob + "]";
+	}
+	
+	
+}
